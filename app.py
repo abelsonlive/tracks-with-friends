94248fb117af05141b9f46ecd3a864b8955ea766
Brian Abelson <brianabelson@Brians-MBP-2.nyc.rr.com>
2016-03-29 00:20:07 -0400

    py3 changes

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-  return render_template('index.html')
+  return(render_template('index.html'))
 
 
 @app.route('/track', methods=['POST'])
 def add_track():
-    print request.files['file']
-    return 'Hello World!'
+    print(request.files['file'])
+    return('Hello World!')
 
 if __name__ == '__main__':
     app.run(debug=True)
